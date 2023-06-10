3539f4b284da43d7ec4e73cf59530390f8db40c3
tanishisherewithhh <120117618+tanishisherewithhh@users.noreply.github.com>
2023-06-10 14:05:10 +0530

    Update DynamicHUDmod.java

--- src/main/java/net/dynamichud/dynamichud/DynamicHUDmod.java
+++ src/main/java/net/dynamichud/dynamichud/DynamicHUDmod.java
@@ -78,7 +78,7 @@ import static net.dynamichud.dynamichud.DynamicHUD.*;
             widgets.add(new ArmorWidget(mc, EquipmentSlot.HEAD, 0.03f, 0.01f, true, TextureHelper.Position.BELOW, () -> text, Color.BLACK));
             widgets.add(new ArmorWidget(mc, EquipmentSlot.LEGS, 0.05f, 0.01f, true, TextureHelper.Position.LEFT, () -> String.valueOf(MinecraftClient.getInstance().getCurrentFps()), Color.WHITE));
 
-            widgets.add(new ItemWidget(mc,() -> mc.player != null ? mc.player.getInventory().getStack(3) : Items.DIAMOND_AXE.getDefaultStack(), 0.15f, 0.15f, true, TextureHelper.Position.ABOVE, () -> "", Color.RED));
+            widgets.add(new ItemWidget(mc,() -> mc.player != null ? mc.player.getInventory().getStack(3) : Items.DIAMOND_AXE.getDefaultStack(), 0.15f, 0.15f, true, TextureHelper.Position.ABOVE, () -> "",()-> Color.RED));
 
     }
 
